c826d84a41335c07cb3794a25d13e16c74715d0d
unknown <zorg99900000@mail.ru>
2019-10-28 19:07:16 +0100

    unit tests

---
+++ src/test/java/com/booking/booking/controller/BookingControllerTest.java
@@ -0,0 +1,24 @@
+package com.booking.booking.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+    @Test
+    void getAvailableRooms() {
+    }
+
+    @Test
+    void createBooking() {
+    }
+
+    @Test
+    void getBookings() {
+    }
+
+    @Test
+    void cancelBooking() {
+    }
+}
---
+++ src/test/java/com/booking/booking/service/BookingServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.booking.booking.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceImplTest {
+
+    @Test
+    void getAvailableRooms() {
+    }
+
+    @Test
+    void getBookingRooms() {
+    }
+
+    @Test
+    void addBooking() {
+    }
+
+    @Test
+    void deleteBooking() {
+    }
+}
